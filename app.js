d0ce0ed825e26c183230c0670a782e30759874f1
yaoyiqun <yaoyiqun@outlook.com>
2017-01-09 15:24:49 +0800

    express4框架生成，导入log4js，passport

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var log4js = require('log4js');
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+//网络访问日志配置
+log4js.configure({
+  appenders: [
+    { type: 'console' },{
+      type: 'file', 
+      filename: 'logs/net.log', 
+      maxLogSize: 1024,
+      backups:4,
+      category: 'normal' 
+    }
+  ],
+  replaceConsole: true
+});
+var logger = log4js.getLogger("normal");
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(logger('dev'));
+//配置日志
+app.use(log4js.connectLogger(logger, {level:log4js.levels.INFO}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
